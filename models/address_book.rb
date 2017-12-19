2a7be40b3cb685842f02954916026da081560fd8
JackCorley <jdcorley@gmail.com>
2017-12-18 21:25:05 -0500

    assignment-5 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,34 +1,34 @@
 require_relative 'entry'
 
 class AddressBook
-	attr_reader :entries
+  attr_reader :entries
 
-	def initialize
-	 @entries = []
-	end 
+  def initialize
+    @entries = []
+  end
 
-def remove_entry(name, phone_number, email)
-		delete_entry = nil
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
 
-		@entries.each do |entry|
-			if name == entry.name && phone_number == entry.phone_number && email == entry.email
-				delete_entry = entry 
-			end 
-		end 
-		@entries.delete(delete_entry)
-	end 
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+    @entries.delete(delete_entry)
+  end
 
-	def add_entry(name, phone_number, email)
+  def add_entry(name, phone_number, email)
 
-		index = 0
-		@entries.each do |entry|
+    index = 0
+    @entries.each do |entry|
 
-			if name < entry.name
-				break
-			end 
-			index+= 1
-		  end 
+      if name < entry.name
+        break
+      end
+      index+= 1
+    end
 
-		  @entries.insert(index, Entry.new(name, phone_number, email))
-		end 
-	end 
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+end
